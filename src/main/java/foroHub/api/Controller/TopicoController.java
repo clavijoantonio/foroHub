e0aca8c549cb8fc9832d43a5b76014e6a0f6296c
antonio clavijo <clavijo1785@gmail.com>
2025-01-11 23:32:59 -0500

    se agrega el metodo get por topico y agrega la post q retorne el objecto creado y el hander

--- src/main/java/foroHub/api/Controller/TopicoController.java
+++ src/main/java/foroHub/api/Controller/TopicoController.java
@@ -1,23 +1,19 @@
 package foroHub.api.Controller;
 
+import java.net.URI;
 import java.util.List;
 
+import foroHub.api.topico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import foroHub.api.topico.DatosListaTopico;
-import foroHub.api.topico.DatosRegistroTopico;
-import foroHub.api.topico.ITopicoRepository;
-import foroHub.api.topico.Topico;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
@@ -29,15 +25,26 @@ public class TopicoController {
 	
 	    @PostMapping
 	    @Transactional
-	    public void registrar(@RequestBody @Valid DatosRegistroTopico datos) {
-
-	        repository.save(new Topico(datos));
+	    public ResponseEntity<DatosRespuestaTopico> registrar(@RequestBody @Valid DatosRegistroTopico datos, UriComponentsBuilder uriComponentsBuilder) {
+			Topico topico =repository.save(new Topico(datos));
+			DatosRespuestaTopico datosRespuestaTopico= new DatosRespuestaTopico(topico.getTitulo(),topico.getMensaje(),
+			                     topico.getAutor(), topico.getStatus(),topico.getFechaCreacion().toString(), topico.getRespuestas());
+			URI url = uriComponentsBuilder.path("/topico/{id}").buildAndExpand(topico.getId()).toUri();
+           return ResponseEntity.created(url).body(datosRespuestaTopico);
 	    }
 	    
 	    @GetMapping
-	    public List<DatosListaTopico> listarTopico() {
-	        return repository.findAll().stream().map(DatosListaTopico::new).toList();
+	    public Page<DatosListaTopico> listarTopico(@PageableDefault(page=0, size=10, sort = {"fechaCreacion"}) Pageable paginacion) {
+	        return repository.findAll(paginacion).map(DatosListaTopico::new);
 	    }
+
+		@GetMapping("/{id}")
+		public ResponseEntity<DatosRespuestaTopico> obtenerTopico(@PathVariable Long id){
+             Topico topico = repository.getReferenceById(id);
+			 var datosTopico = new DatosRespuestaTopico(topico.getTitulo(),topico.getMensaje(),
+					topico.getAutor(), topico.getStatus(),topico.getFechaCreacion().toString(), topico.getRespuestas());
+			 return ResponseEntity.ok(datosTopico);
+		}
 	    
 	    
 }
---
+++ src/main/java/foroHub/api/topico/DatosRespuestaTopico.java
@@ -0,0 +1,16 @@
+package foroHub.api.topico;
+
+public record DatosRespuestaTopico(
+        String titulo,
+        String mensaje,
+        String fechaCreacion,
+        String status,
+        String autor,
+        String respuestas
+) {
+
+    public DatosRespuestaTopico(Topico topico){
+        this(topico.getTitulo(), topico.getMensaje(), topico.getAutor(), topico.getStatus(),topico.getFechaCreacion().toString(), topico.getRespuestas());
+    }
+
+}
